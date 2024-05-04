97fa8213fd6cc3bab2cbce795e07df36112db2da
marchjeff145@gmail.com <marchjeff145@gmail.com>
2024-05-04 12:23:48 +0700

    add database model

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,24 +1,146 @@
-// type FieldErrors = {
-//   [key in string]?: string[];
-// };
+interface BaseAuditableEntity {
+  id: string;
+  created: Date;
+  createdBy: string;
+  LastModified: Date;
+  lastModifiedBy: string;
+  deletedDate: Date;
+  deletedBy: string;
 
-// type FormState<TResponse> = {
-//   isSuccess: boolean;
-//   message?: string;
-//   result?: TResponse;
-//   fieldErrors?: FieldErrors;
-//   errorMessages?: string[];
-//   responseType: "server" | "validation";
-// };
+  isDeleted: boolean;
+}
 
-type SessionPayload = {
-  accessToken: string;
-  refreshToken: string;
-  expiresIn: number;
-  // userId: string;
-  // expiresAt: Date;
-  // userName: string;
-  // isEmailConfirmed: boolean;
-};
+interface Indexable {
+  orderIndex: number;
+}
 
-export type { SessionPayload };
+interface Activity extends BaseAuditableEntity, Indexable {
+  activityTypeId: string;
+  activityType: object;
+  topicId: String;
+  topic: object;
+  documents: Array<Document>;
+  videos: Array<Video>;
+  worksheets: Array<Worksheet>;
+}
+
+interface ActivityType extends BaseAuditableEntity {
+  name: string;
+  activities: Array<Activity>;
+}
+
+interface Chapter extends BaseAuditableEntity {
+  title: string;
+  description: string;
+  courseId: string;
+  course: Course;
+  topics: Array<Topic>;
+}
+
+interface Course extends BaseAuditableEntity {
+  title: string;
+  description: string;
+  subjectId: string;
+  subject: Subject;
+  chapters: Array<Chapter>;
+  studentInCourse: Array<StudentInCourse>;
+}
+
+interface Document extends BaseAuditableEntity {
+  title: string;
+  content: string;
+  author: string;
+  activityId: string;
+  activity: Activity;
+}
+
+interface Question extends BaseAuditableEntity, Indexable {
+  text: string;
+  imageUrl: string;
+  topicId: string;
+  topic: Topic;
+  questionLevelId: string;
+  questionLevel: QuestionLevel;
+  questionTypeId: string;
+  questionType: QuestionType;
+  questionAnswers: Array<QuestionAnswer>;
+  worksheetQuestions: Array<WorksheetQuestion>;
+}
+
+interface QuestionAnswer extends BaseAuditableEntity, Indexable {
+  text: string;
+  isCorrect: boolean;
+  questionId: string;
+  question: Question;
+}
+
+interface QuestionLevel extends BaseAuditableEntity {
+  name: string;
+  questions: Array<Question>;
+}
+
+interface QuestionType extends BaseAuditableEntity {
+  name: string;
+  questions: Array<Question>;
+}
+
+interface StudentInCourse extends BaseAuditableEntity {
+  studentId: string;
+  // missing student object here
+  courseId: string;
+  course: Course;
+}
+
+interface Subject extends BaseAuditableEntity {
+  title: string;
+  description: string;
+  courses: Array<Course>;
+}
+
+interface Topic extends BaseAuditableEntity {
+  title: string;
+  description: string;
+  chapterId: string;
+  chapter: Chapter;
+  activities: Array<Activity>;
+}
+
+interface Video extends BaseAuditableEntity {
+  title: string;
+  url: string;
+  activityId: string;
+  activity: Activity;
+}
+
+interface Worksheet extends BaseAuditableEntity {
+  title: string;
+  description: string;
+  activityId: string;
+  activity: Activity;
+  worksheetTemplateId?: string;
+  worksheetTemplate?: WorksheetTemplate;
+  worksheetQuestions: Array<WorksheetQuestion>;
+}
+
+interface WorksheetQuestion extends BaseAuditableEntity {
+  worksheetId?: string;
+  worksheet?: Worksheet;
+  questionId: string;
+  question: Question;
+}
+
+interface WorksheetTemplate extends BaseAuditableEntity {
+  classification: 0 | 1 | 2; // subject | chapter | topic
+  easyQuestionCount: number;
+  normalQuestionCount: number;
+  hardQuestionCount: number;
+  totalQuestionCount: number;
+  suffle: boolean;
+  subjectId: string;
+  subject: Subject;
+  chapterId: string;
+  chapter: Chapter;
+  topicId: String;
+  topic: object;
+  worksheets: Array<Worksheet>;
+}
