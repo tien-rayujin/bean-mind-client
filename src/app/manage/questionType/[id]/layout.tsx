8947a4582613be615514f6ee1995c4ef9b2603b5
tien-rayujin <tien.rayujin.145@gmail.com>
2024-05-23 16:31:05 +0700

    Styling QuestionType page

--- src/app/manage/questionType/[id]/layout.tsx
+++ src/app/manage/questionType/[id]/layout.tsx
@@ -1,56 +1,18 @@
 import Breadcrumb from "@/components/Breadcrumb";
-import { StyButton } from "@/components/Button";
-import { GetQuestionTypeRequestHandler } from "@/lib/services/questionType/Handlers";
-import Link from "next/link";
-import { notFound } from "next/navigation";
 import { PropsWithChildren } from "react";
-import { FaPen, FaRedo, FaTrash } from "react-icons/fa";
 
 interface QuestionTypeDetailLayoutProps extends PropsWithChildren {
   modal: React.ReactNode;
-  params: { id: string };
 }
 
 const Layout: React.FC<QuestionTypeDetailLayoutProps> = async (props) => {
-  const { children, modal, params } = props;
-  const id = params.id;
-
-  const questionType = (await GetQuestionTypeRequestHandler(id)).data;
-
-  if (!questionType) return notFound();
-
-  const { isDeleted } = questionType;
-
+  const { children, modal } = props;
   return (
     <div className="relative h-full">
       {modal}
 
       <div className="flex h-full max-h-full flex-col overflow-y-hidden">
         <Breadcrumb pageName="QuestionType Detail" />
-
-        <div className="mb-4 flex items-center justify-end gap-2.5">
-          <Link href={`${id}/update`} className="z-10">
-            <StyButton extras="flex items-center gap-2">
-              <FaPen />
-              <span>Edit</span>
-            </StyButton>
-          </Link>
-          {!isDeleted ? (
-            <Link href={`${id}/delete`} className="z-10">
-              <StyButton extras="flex items-center gap-2 !bg-accent">
-                <FaTrash />
-                <span>Delete</span>
-              </StyButton>
-            </Link>
-          ) : (
-            <Link href={`${id}/restore`} className="z-10">
-              <StyButton extras="flex items-center gap-2 !bg-accent">
-                <FaRedo />
-                <span>Restore</span>
-              </StyButton>
-            </Link>
-          )}
-        </div>
 
         <div className="h-fit max-h-full flex-1">{children}</div>
       </div>
--- src/app/manage/questionType/page.tsx
+++ src/app/manage/questionType/page.tsx
@@ -1,7 +1,12 @@
 import Breadcrumb from "@/components/Breadcrumb";
-import { FilterQuestionTypeButton } from "./components/Button";
 import SearchBar from "@/components/SearchBar";
 import QuestionTypeTable from "./components/Table";
+import {
+  CreateButton,
+  FilterButton,
+  ReloadButton,
+} from "@/components/Form/Button";
+import Link from "next/link";
 
 interface QuestionTypePageProps {
   searchParams: {
@@ -22,12 +27,19 @@ const Page: React.FC<QuestionTypePageProps> = async (props) => {
     <div className="relative h-full">
       <Breadcrumb pageName="QuestionType" />
 
-      {/* Search area */}
-      <div className="mb-4 flex items-center gap-4">
-        <SearchBar extras="w-full" />
-        {/* Filter button */}
-        <FilterQuestionTypeButton />
-      </div>
+      <section className="mb-4 w-full rounded-md bg-background/30 p-4 shadow-md">
+        {/* Search area */}
+        <div className="mb-4 flex items-center gap-4">
+          <SearchBar extras="flex-1" />
+          <Link href={"topic/create"}>
+            <CreateButton text="Create topic" extras="h-12" />
+          </Link>
+          <FilterButton />
+          <ReloadButton />
+        </div>
+
+        {/* Filter area */}
+      </section>
 
       <QuestionTypeTable
         pageIndex={pageIndex}
