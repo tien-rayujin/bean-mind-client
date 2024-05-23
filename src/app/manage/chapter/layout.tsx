374b01a7097d2a0aaaa86555ce9bda15bccb18b9
tien-rayujin <tien.rayujin.145@gmail.com>
2024-05-23 16:23:45 +0700

    Styling Chapter page

--- src/app/manage/chapter/layout.tsx
+++ src/app/manage/chapter/layout.tsx
@@ -1,7 +1,4 @@
-import { StyButton } from "@/components/Button";
-import Link from "next/link";
 import { PropsWithChildren } from "react";
-import { FaPlus } from "react-icons/fa";
 
 interface ChapterLayoutProps extends PropsWithChildren {
   modal: React.ReactNode;
@@ -12,12 +9,6 @@ const Layout: React.FC<ChapterLayoutProps> = (props) => {
 
   return (
     <div className="relative h-full">
-      <Link href={`chapter/create`}>
-        <StyButton extras="absolute bottom-0 right-0 rounded-full w-12 h-12 z-10">
-          <FaPlus />
-        </StyButton>
-      </Link>
-
       {modal}
       {children}
     </div>
