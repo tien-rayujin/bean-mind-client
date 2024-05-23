f6ab90135f31ccdf6f82c94b3329a8f539738537
tien-rayujin <tien.rayujin.145@gmail.com>
2024-05-23 16:33:44 +0700

    Styling Activity page

--- src/app/manage/activity/[id]/page.tsx
+++ src/app/manage/activity/[id]/page.tsx
@@ -1,4 +1,9 @@
 import { StyButton } from "@/components/Button";
+import {
+  UpdateButton,
+  DeleteButton,
+  RestoreButton,
+} from "@/components/Form/Button";
 import { GetActivityRequestHandler } from "@/lib/services/activity/Handlers";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -19,65 +24,82 @@ const Page: React.FC<PageProps> = async (props) => {
     activity;
 
   return (
-    <div className="col-span-12">
-      <div className="relative bg-background p-8 leading-relaxed">
-        <p>
-          <span className="font-semibold tracking-wide text-primary">
-            Type:{" "}
-          </span>
-          {activityType.name}
-        </p>
-        <p className="block max-w-[50%]">
-          <span className="font-semibold tracking-wide text-primary">
-            Topic:{" "}
-          </span>
-          {topic.title}
-        </p>
-        <div className="flex items-center justify-start gap-2.5">
-          <span className="font-semibold tracking-wide text-primary">
-            Status:{" "}
-          </span>
-          <span>{isDeleted ? "Disabled" : "Enabled"}</span>
-        </div>
+    <>
+      <div className="col-span-12">
+        <div className="relative bg-background p-8 leading-relaxed">
+          <p>
+            <span className="font-semibold tracking-wide text-primary">
+              Type:{" "}
+            </span>
+            {activityType.name}
+          </p>
+          <p className="block max-w-[50%]">
+            <span className="font-semibold tracking-wide text-primary">
+              Topic:{" "}
+            </span>
+            {topic.title}
+          </p>
+          <div className="flex items-center justify-start gap-2.5">
+            <span className="font-semibold tracking-wide text-primary">
+              Status:{" "}
+            </span>
+            <span>{isDeleted ? "Disabled" : "Enabled"}</span>
+          </div>
 
-        {documents &&
-          documents.map((item, index) => {
-            return (
-              <p key={item.id} className="block max-w-[50%]">
-                <span className="font-semibold tracking-wide text-primary">
-                  Document {index + 1}:{" "}
-                </span>
-                {item.title}
-              </p>
-            );
-          })}
-        {videos &&
-          videos.map((item, index) => {
-            return (
-              <p key={item.id} className="block max-w-[50%]">
-                <span className="font-semibold tracking-wide text-primary">
-                  Videos {index + 1}:{" "}
-                </span>
-                {item.title}
-              </p>
-            );
-          })}
-        {worksheets &&
-          worksheets.map((item, index) => {
-            return (
-              <p key={item.id} className="block max-w-[50%]">
-                <span className="font-semibold tracking-wide text-primary">
-                  Worksheet {index + 1}:{" "}
-                </span>
-                {JSON.stringify(item)}
-                <Link href={`/manage/worksheet/${item.id}`}>
-                  <StyButton>Jump to Worksheet</StyButton>
-                </Link>
-              </p>
-            );
-          })}
+          {documents &&
+            documents.map((item, index) => {
+              return (
+                <p key={item.id} className="block max-w-[50%]">
+                  <span className="font-semibold tracking-wide text-primary">
+                    Document {index + 1}:{" "}
+                  </span>
+                  {item.title}
+                </p>
+              );
+            })}
+          {videos &&
+            videos.map((item, index) => {
+              return (
+                <p key={item.id} className="block max-w-[50%]">
+                  <span className="font-semibold tracking-wide text-primary">
+                    Videos {index + 1}:{" "}
+                  </span>
+                  {item.title}
+                </p>
+              );
+            })}
+          {worksheets &&
+            worksheets.map((item, index) => {
+              return (
+                <p key={item.id} className="block max-w-[50%]">
+                  <span className="font-semibold tracking-wide text-primary">
+                    Worksheet {index + 1}:{" "}
+                  </span>
+                  {JSON.stringify(item)}
+                  <Link href={`/manage/worksheet/${item.id}`}>
+                    <StyButton>Jump to Worksheet</StyButton>
+                  </Link>
+                </p>
+              );
+            })}
+        </div>
       </div>
-    </div>
+
+      <div className="mt-4 flex items-center justify-end gap-2.5">
+        <Link href={`${id}/update`} className="z-10">
+          <UpdateButton />
+        </Link>
+        {!isDeleted ? (
+          <Link href={`${id}/delete`} className="z-10">
+            <DeleteButton />
+          </Link>
+        ) : (
+          <Link href={`${id}/restore`} className="z-10">
+            <RestoreButton />
+          </Link>
+        )}
+      </div>
+    </>
   );
 };
 
