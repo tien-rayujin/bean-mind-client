8d60bb89a69c01eeaf2b50104c618c813039a62b
tien-rayujin <tien.rayujin.145@gmail.com>
2024-05-24 17:08:24 +0700

    Create BaseFilter section

--- src/app/manage/subject/components/Filter.tsx
+++ src/app/manage/subject/components/Filter.tsx
@@ -1,59 +1,20 @@
-"use client";
+import { BaseFilter, FilterItemProps } from "@/components/Filter";
 
-import { StyFormSelect } from "@/components/Form/FormInput";
-import { createPageUrl } from "@/lib/utils";
-import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { ChangeEvent, useEffect, useState } from "react";
+const subjectFilterDefinition: FilterItemProps[] = [
+  {
+    name: "isDeleted",
+    datas: [
+      { display: "All", value: "all" },
+      { display: "Active", value: "active" },
+      { display: "In-active", value: "inactive" },
+    ],
+    label: "Status",
+  },
+];
 
 interface SubjectFilterProps {}
 
 const SubjectFilter: React.FC<SubjectFilterProps> = (props) => {
-  const [terms, setTerms] = useState<Record<string, string>>({});
-
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const nextUrl = createPageUrl({
-      terms,
-      pathname,
-      searchParams,
-    });
-    // change the current url
-    router.replace(nextUrl);
-  }, [terms]);
-
-  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const nextTerms = {
-      ...terms,
-      [e.target.name]: e.target.value,
-    };
-    setTerms(nextTerms);
-  };
-
-  return (
-    <div className="grid grid-cols-6 gap-4">
-      <div>
-        <StyFormSelect
-          name="isDeleted"
-          displayProp={"key"}
-          valueProp={"value"}
-          showLabel={false}
-          onChange={handleChange}
-          datas={[
-            { key: "All", value: "all" },
-            { key: "Active", value: "active" },
-            { key: "In-active", value: "inactive" },
-          ]}
-          value={searchParams.get("isDeleted") || ""}
-          label={"Status"}
-          placeholder="Select Status"
-          extras="py-1"
-        />
-      </div>
-    </div>
-  );
+  return <BaseFilter dataList={subjectFilterDefinition} />;
 };
-
 export { SubjectFilter };
---
+++ src/components/Filter.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { useState, ChangeEvent, useEffect } from "react";
+import { StyFormSelect } from "./Form/FormInput";
+import { createPageUrl } from "@/lib/utils";
+import { useRouter } from "next/router";
+import clsx from "clsx";
+
+interface BaseFilterProps {
+  dataList: FilterItemProps[];
+}
+
+const BaseFilter: React.FC<BaseFilterProps> = (props) => {
+  const { dataList } = props;
+  const [terms, setTerms] = useState<Record<string, string>>({});
+
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const nextTerms = {
+      ...terms,
+      [e.target.name]: e.target.value,
+    };
+    setTerms(nextTerms);
+  };
+
+  useEffect(() => {
+    const nextUrl = createPageUrl({
+      terms,
+      pathname,
+      searchParams,
+    });
+    router.replace(nextUrl);
+  }, [terms, router, pathname, searchParams]);
+
+  return (
+    <div className={clsx("grid grid-cols-6 gap-4")}>
+      {dataList &&
+        dataList.map((data, idx) => {
+          const key = "__select__" + data.label + idx;
+
+          return (
+            <FilterItem
+              {...data}
+              key={key}
+              value={searchParams.get(data.name) || ""}
+              onChange={handleChange}
+            />
+          );
+        })}
+    </div>
+  );
+};
+
+type FilterDataItem = {
+  display: string;
+  value: string;
+};
+
+interface FilterItemProps {
+  name: string;
+  displayProp?: keyof FilterDataItem;
+  valueProp?: keyof FilterDataItem;
+  onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
+  datas: FilterDataItem[];
+  value?: string;
+  label: string;
+  placeholder?: string;
+}
+
+const FilterItem: React.FC<FilterItemProps> = (props) => {
+  const mappedProps = {
+    ...props,
+    displayProp: props.displayProp || "display",
+    valueProp: props.displayProp || "value",
+    value: props.value || "",
+    placeholder:
+      props.placeholder || `Select ${props.label.toLocaleLowerCase()}`,
+  };
+
+  return (
+    <div>
+      <StyFormSelect {...mappedProps} extras="py-1" showLabel={false} />
+    </div>
+  );
+};
+
+export { type FilterItemProps, BaseFilter };
